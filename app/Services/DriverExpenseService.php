86e2ec51acb5fb1aa71c8b9a09054d1aeb4b455c
Vaidas <61739600+Vaidelis@users.noreply.github.com>
2022-12-27 00:58:19 +0200

    Comments

--- app/Services/DriverExpenseService.php
+++ app/Services/DriverExpenseService.php
@@ -9,14 +9,14 @@ class DriverExpenseService
         $result = [];
 
         // Write your logic here.
-        $driver1 = 0; // 1st driver expensis sum
-        $driver2 = 0; // 2st driver expensis sum
+        $driver1 = 0; //sum of 1st driver expensis
+        $driver2 = 0; //sum of 2st driver expensis
         for($i = 0; $i < count($expenses); $i++) {
 
-            $divine = round($driver1 - $driver2, 2); // differences between drivers expensis
+            $divine = round($driver1 - $driver2, 2); // differences between drivers expenses
             if (round($driver1, 2) == round($driver2,2) || $divine == 0.01) { //if drivers expensis sum is equal or differences is 0.01 euros
                 $dividednumber = array_values($expenses)[$i] / 2;
-                if (round($dividednumber, 2) == $dividednumber) { //check if decimal has 2 numbers after decimal point
+                if (round($dividednumber, 2) == $dividednumber) { //checks whether if decimal has 2 numbers after decimal point
                     $result[array_keys($expenses)[$i]] = array('key1' => array_values($expenses)[$i] / 2, 'name1' => $drivers[0],
                         'key2' => array_values($expenses)[$i] / 2, 'name2' => $drivers[1]);
                     $driver1 = $driver1 + array_values($expenses)[$i] / 2;
@@ -39,17 +39,17 @@ class DriverExpenseService
             }
             else{ //if drivers expenses is not equal
                 $dividednumber = array_values($expenses)[$i] / 2;
-                if (round($dividednumber, 2) == $dividednumber) { //check if expensis is divined by twy and has 2 numbers after decimal point
+                if (round($dividednumber, 2) == $dividednumber) { //checks whether if expenses is divided by two and has 2 numbers after decimal point
                     $result[array_keys($expenses)[$i]] = array('key1' => array_values($expenses)[$i] / 2 - 0.01, 'name1' => $drivers[0],
                         'key2' => array_values($expenses)[$i] / 2 + 0.01, 'name2' => $drivers[1]);
                     $driver1 = $driver1 + array_values($expenses)[$i] / 2 - 0.01;
-                    $driver2 = $driver2 + array_values($expenses)[$i] / 2 + 0.01; //expensis sum is not equal, so 2nd driver gets 0.01 euros more than 1st driver
+                    $driver2 = $driver2 + array_values($expenses)[$i] / 2 + 0.01; //expenses sum is not equal, so 2nd driver gets 0.01 euros more than 1st driver
                 } else {
                     $roundednumber = round(array_values($expenses)[$i] / 2, 2);
                     $result[array_keys($expenses)[$i]] = array('key1' => $roundednumber - 0.01, 'name1' => $drivers[0],
                         'key2' => $roundednumber, 'name2' => $drivers[1]);
                     $driver1 = $driver1 + $roundednumber - 0.01;
-                    $driver2 = $driver2 + $roundednumber; //expensis sum is not equal, so first driver gets 0.01 euros less
+                    $driver2 = $driver2 + $roundednumber; //expenses sum is not equal, to first driver gets 0.01 euros less
                 }
 
             }
